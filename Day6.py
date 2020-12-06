09e01bfdc3738e11689cfb7abec099dd75e8ed1e
Adam Mróz <adam.michal.mroz@gmail.com>
2020-12-06 13:55:32 +0100

    Day 6 - part 2

--- Day6.py
+++ Day6.py
@@ -1,22 +1,46 @@
-def task1():
+def task1(lines):
     result = 0
 
-    with open('./input-day6', 'r') as f:
-        lines = f.read().splitlines()
+    unique_chars = set()
 
-        unique_chars = set()
+    for line in lines:
+        if not line:
+            result += len(unique_chars)
+            unique_chars = set()
+        else:
+            unique_chars.update(list(line))
 
-        for line in lines:
-            if not line:
-                result += len(unique_chars)
-                unique_chars = set()
-            else:
-                unique_chars.update(list(line))
+    result += len(unique_chars)
 
-        result += len(unique_chars)
+    return result
+
+
+def task2(lines):
+    from collections import Counter
+
+    result = 0
+
+    group_frequenices = Counter()
+    lines_in_group = 0
+
+    for line in lines:
+        if not line:
+            result += sum(1 for _,
+                          v in group_frequenices.items() if v == lines_in_group)
+            group_frequenices.clear()
+            lines_in_group = 0
+        else:
+            group_frequenices += Counter(line)
+            lines_in_group += 1
+
+    result += sum(1 for _, v in group_frequenices.items()
+                  if v == lines_in_group)
 
     return result
 
 
 if __name__ == "__main__":
-    print(task1())
+    with open('./input-day6', 'r') as f:
+        lines = f.read().splitlines()
+        print(task1(lines))
+        print(task2(lines))
