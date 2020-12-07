b46b6f13ef3434931ae5851d7f0d823d769e311b
Adam Mróz <adam.michal.mroz@gmail.com>
2020-12-07 20:29:23 +0100

    Day 7 - part 2

--- Day7.py
+++ Day7.py
@@ -8,12 +8,21 @@ def can_contain_shiny_gold(bag, policies):
     if bag not in policies or not policies[bag]:
         return False
 
-    childs = policies[bag]
+    childs = [color for (_, color) in policies[bag]]
 
     if 'shiny gold' in childs:
         return True
 
     return any([can_contain_shiny_gold(child, policies) for child in childs])
+
+
+def child_bags(bags, policies):
+    if not bags:
+        return 0
+
+    current_bag = bags[0]
+
+    return int(current_bag[0]) * (1 + child_bags(policies[current_bag[1]], policies)) + child_bags(bags[1:], policies)
 
 
 def parse_policies():
@@ -24,7 +33,7 @@ def parse_policies():
         line = l.strip()
         parent = line.split('bags')[0].strip()
         childs = re.findall(
-            r'\d+ ([a-z ]+) bag[s,.]*', line.split('contain')[1])
+            r'(\d+) ([a-z ]+) bag[s,.]*', line.split('contain')[1])
 
         policies[parent] = childs
 
@@ -34,3 +43,4 @@ def parse_policies():
 
 bags, policies = parse_policies()
 print(sum([can_contain_shiny_gold(bag, policies) for bag in bags]))
+print(child_bags(policies['shiny gold'], policies))
