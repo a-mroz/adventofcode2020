8e1282f0a19ddf8c610770d68cb8ce65eb1e1125
Adam Mróz <adam.michal.mroz@gmail.com>
2020-12-10 18:21:56 +0100

    Day 10 - part 1 - improved solution

--- Day10.py
+++ Day10.py
@@ -9,54 +9,40 @@ def parse():
     for l in fileinput.input():
         jolts.append(int(l.strip()))
 
-    your_adapter = max(jolts) + 3
-    jolts.append(your_adapter)
-
     return jolts
 
 
 def task1(jolts):
-    jolts_with_differences_1 = []
-    jolts_with_differences_2 = []
-    jolts_with_differences_3 = []
+    diff_1 = 0
+    diff_3 = 0
 
-    already_processed = set()
+    for i in range(len(jolts) - 1):
+        diff = jolts[i+1] - jolts[i]
 
-    def rec(current_jolt, diff, acc):
-        print(current_jolt, diff, acc)
-        if current_jolt in already_processed:
-            return
+        if diff == 1:
+            diff_1 += 1
+        elif diff == 3:
+            diff_3 += 1
 
-        candidate = current_jolt + diff
-
-        print(candidate)
-
-        if candidate in jolts:
-            print("in jolts", candidate)
-            acc.append(candidate)
-            already_processed.add(current_jolt)
-            rec(candidate, 1, jolts_with_differences_1)
-            rec(candidate, 2, jolts_with_differences_2)
-            rec(candidate, 3, jolts_with_differences_3)
-
-    rec(0, 1, jolts_with_differences_1)
-    rec(0, 2, jolts_with_differences_2)
-    rec(0, 3, jolts_with_differences_3)
-
-    return jolts_with_differences_1, jolts_with_differences_2, jolts_with_differences_3
+    return diff_1 * diff_3
 
 
 def task2(numbers):
     pass
 
 
-jolts = parse()
-
-
 # Part 1
-jolts_with_differences_1, jolts_with_differences_2, jolts_with_differences_3 = task1(
-    jolts)
-print(len(jolts_with_differences_1) * len(jolts_with_differences_3))
+jolts = parse()
+starting_point = 0
+your_adapter = max(jolts) + 3
+
+jolts.append(starting_point)
+jolts.append(your_adapter)
+
+jolts = sorted(jolts)
+
+print(task1(jolts))
+
 
 # Part 2
 print(task2(jolts))
