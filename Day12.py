3c327fef3593a28fa06d1865179d6235210a8635
Adam Mróz <adam.michal.mroz@gmail.com>
2020-12-12 12:53:52 +0100

    Day 12 - part 2

--- Day12.py
+++ Day12.py
@@ -7,29 +7,29 @@ def parse():
     return [(l[0], int(l.strip()[1:])) for l in fileinput.input()]
 
 
-def rotate_right(dir):
-    if dir == [0, 1]:
-        return [-1, 0]
-    if dir == [-1, 0]:
-        return [0, -1]
-    if dir == [0, -1]:
-        return [1, 0]
-    if dir == [1, 0]:
-        return [0, 1]
-
-
-def rotate_left(dir):
-    if dir == [0, 1]:
-        return [1, 0]
-    if dir == [1, 0]:
-        return [0, -1]
-    if dir == [0, -1]:
-        return [-1, 0]
-    if dir == [-1, 0]:
-        return [0, 1]
-
-
 def task1(instructions):
+    def rotate_right(dir):
+        if dir == [0, 1]:
+            return [-1, 0]
+        if dir == [-1, 0]:
+            return [0, -1]
+        if dir == [0, -1]:
+            return [1, 0]
+        if dir == [1, 0]:
+            return [0, 1]
+        print('Unrecognized direction:', dir)
+
+    def rotate_left(dir):
+        if dir == [0, 1]:
+            return [1, 0]
+        if dir == [1, 0]:
+            return [0, -1]
+        if dir == [0, -1]:
+            return [-1, 0]
+        if dir == [-1, 0]:
+            return [0, 1]
+        print('Unrecognized direction:', dir)
+
     north = 0
     east = 0
 
@@ -49,10 +49,10 @@ def task1(instructions):
         elif dir == 'W':
             east -= count
         elif dir == 'R':
-            for i in range(int(count / 90)):
+            for i in range(count // 90):
                 current_dir = rotate_right(current_dir)
         elif dir == 'L':
-            for i in range(int(count / 90)):
+            for i in range(count // 90):
                 current_dir = rotate_left(current_dir)
         else:
             print('Unrecognized instruction')
@@ -60,7 +60,38 @@ def task1(instructions):
 
 
 def task2(grid):
-    pass
+    ship_north = 0
+    ship_east = 0
+
+    waypoint_north = 1
+    waypoint_east = 10
+
+    for dir, count in instructions:
+        if dir == 'F':
+            ship_north += waypoint_north * count
+            ship_east += waypoint_east * count
+        elif dir == 'N':
+            waypoint_north += count
+        elif dir == 'S':
+            waypoint_north -= count
+        elif dir == 'E':
+            waypoint_east += count
+        elif dir == 'W':
+            waypoint_east -= count
+        elif dir == 'R':
+            for i in range(count // 90):
+                waypoint_north, waypoint_east = waypoint_east, waypoint_north
+                waypoint_north *= -1
+
+        elif dir == 'L':
+            for i in range(4 - (count // 90)):
+                waypoint_north, waypoint_east = waypoint_east, waypoint_north
+                waypoint_north *= -1
+
+        else:
+            print('Unrecognized instruction')
+
+    return abs(ship_north) + abs(ship_east)
 
 
 # Part 1
