68e536f4fee58cdf4908bf22cd2c8591fec5ca96
Adam Mróz <adam.michal.mroz@gmail.com>
2020-12-14 08:33:20 +0100

    Day 14 - part 2

--- Day14.py
+++ Day14.py
@@ -36,7 +36,49 @@ def task1(instr):
 
 
 def task2(instr):
-    pass
+
+    def all_addresses(acc, addr, mask, i):
+        while i < len(mask) and mask[i] != 'X':
+            i += 1
+
+        if i >= len(mask):
+            acc.append(int(''.join(addr), 2))
+            return
+
+        c1 = copy.deepcopy(addr)
+        c1[i] = '0'
+
+        c2 = copy.deepcopy(addr)
+        c2[i] = '1'
+
+        all_addresses(acc, c1, mask, i + 1)
+        all_addresses(acc, c2, mask, i + 1)
+
+    mask = None
+    mem = {}
+
+    for i, v in instr:
+        if i.startswith('mask'):
+            mask = v.strip()
+        elif i.startswith('mem'):
+            val = list(str.zfill(bin(int(i[4:len(i)-2]))[2:], 36))
+
+            for i, m in enumerate(mask):
+                if m == '0':
+                    continue
+                if m == '1':
+                    val[i] = m
+
+            addresses = []
+            all_addresses(addresses, val, mask, 0)
+
+            for addr in addresses:
+                mem[addr] = int(v)
+        else:
+            print(i)
+            assert False
+
+    return sum(mem.values())
 
 
 # Part 1
